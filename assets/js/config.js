6dd19d415f5c97889ab14933a7088369486fe138
TigoAmigo <TigoAmigo@users.noreply.github.com>
2026-03-25 00:46:34 +0300

    Add Supabase production key

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,6 @@ window.APP_CONFIG = {
   siteUrl: 'https://tigoamigo.github.io/mirror-trainer-landing/',
   contactTelegramUrl: 'https://t.me/your_handle',
   contactTelegramLabel: '@your_handle',
-  supabaseUrl: '',
-  supabaseAnonKey: '',
+  supabaseUrl: 'https://flnhlkqxoumjvwtzkygq.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsbmhsa3F4b3VtanZ3dHpreWdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNzU1NjYsImV4cCI6MjA4OTk1MTU2Nn0.t2s1nE7mQa8WoqVqtUheigOl1xfiZvWd_OEwm2Y3dPo',
 };
